bf407e2dff61307d90533320f4f3e67990af8760
MelissaG <gthmel001@myuct.ac.za>
2024-02-23 16:32:52 +0200

    Code works when using only 1 port number server side. When I introduced UDP things and different ports, the server has a hard time. Remove the UDP stuff if you want something working, but separate ports are needed. Check To:Do.

--- server.py
+++ server.py
@@ -7,43 +7,47 @@ print('We are using the server to capatalisze a sentence sent in from the client
 
 #Define variables
 HEADER = 64     #Header of 64 bytes for the first msg
-PORT = 12000
+CONNECTIONPORT = 12000
+DATAPORT = 5000
 FORMAT = 'utf-8'
 DISCONNECT_PROTOCOL = "!DISCONNECT"
-
-
-#Part 1
-#Connect with TCP socket
-#TCP connection stays open for new clients
+SERVER_NAME = "Server"
 
 #Handles the threading of each client connection running at the same time so the server can receive ina parallel way
 def handle_client(connectionSocket,addr):       
-    print(f"[NEW CONNECTIONS] {addr} connected")
+    print(f"[NEW CONNECTION] {addr} connected")
+    
+    #TO:DO send new port number here, because the UDP socket isn't receiving properly
+    #The UDP socket probably shouldn't be here and I'm sure I can resolve that when
+    #The send/receive protocol is done and the new port is sent
+    
+    #Create a new UDP socket with a new port for each connected client
+    dataSocket = socket(AF_INET,SOCK_DGRAM)
+    dataSocket.bind(('', DATAPORT))
     connected = True
     while connected:
-        msg_length = connectionSocket.recv(HEADER).decode(FORMAT) 
-        if msg_length:      #Check if valid message
-            print(msg_length)
-            msg_length = int(msg_length)
-            msg = connectionSocket.recv(msg_length).decode(FORMAT)
-            
+        msg,clientAddress = dataSocket.recvfrom(2048)
+        print(msg)
+        if msg:      #Check if valid message      
+            print('we do have a valid message')      
             if msg == DISCONNECT_PROTOCOL:
                 connected = False
             capitalizedSentence = msg.upper() 
-            connectionSocket.send(capitalizedSentence.encode(FORMAT))
+            dataSocket.sendto(capitalizedSentence.encode(FORMAT),clientAddress)
     
+    dataSocket.close()
     connectionSocket.close()
-        
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-2}")
+
 
 def start_server():
-    serverPort = PORT
     serverSocket = socket(AF_INET,SOCK_STREAM) 
-    serverSocket.bind(("",serverPort)) 
+    serverSocket.bind(("",CONNECTIONPORT)) #Connect with TCP socket
     serverSocket.listen(1)
-    print(f"[LISTENING] Server is listening on {PORT}")
+    print(f"[LISTENING] Server is listening on {CONNECTIONPORT}")
     print('The server is ready to recieve client connection requests')
     while True:
-        connectionSocket, addr = serverSocket.accept()
+        connectionSocket, addr = serverSocket.accept()  #TCP connection stays open for new clients
         thread = threading.Thread(target=handle_client, args=(connectionSocket,addr))
         thread.start()
         # -1 to exclude server thread
@@ -52,6 +56,3 @@ def start_server():
 
 
 start_server()
-#Part 2
-#Create a new UDP socket with a new port for each connected client
-#Data is transferred here
