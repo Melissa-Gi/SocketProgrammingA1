46c8ff1bb622f786b76c2346ac3ca7174b54b265
MelissaG <gthmel001@myuct.ac.za>
2024-02-28 16:11:21 +0200

    Fixed disconnect with error messages (but they don't actually change the output)

--- client.py
+++ client.py
@@ -7,22 +7,28 @@ ownClientPort = 13000     #for now
 ownClientName = ''
 serverIP = gethostbyname(gethostname())
 
-def receive_texts(udpSocket,myName,otherName):
+def receive_texts(udpSocket,otherName,myName):
     while True: #Controls termination of a client to client messaging
         clientMessage, address = udpSocket.recvfrom(1024)
-        print(myName,':',clientMessage.decode(FORMAT))
-        if clientMessage == DISCONNECT_PROTOCOL:
-            print(otherName, 'is no longer in the chat. You might want to disconnect')
-            
+        if clientMessage.decode(FORMAT) == DISCONNECT_PROTOCOL:
+            print(otherName,'has left the chat. You might want to disconnect yourself.')
+            break
+        else:
+            print(myName,':',clientMessage.decode(FORMAT))
+    interface()
     
 def send_texts(udpSocket,otherName,portNum):
+    global ownClientPort
     while True: #Controls termination of a client to client messaging
             myMessage = input()
             if myMessage == DISCONNECT_PROTOCOL:
                 udpSocket.sendto(myMessage.encode(FORMAT),('localhost',portNum))
+                udpSocket.sendto(myMessage.encode(FORMAT),('localhost',ownClientPort))
                 break
             if myMessage == '/accept':
                 udpSocket.sendto(myMessage.encode(FORMAT),('localhost',12001))
+                udpSocket.sendto(DISCONNECT_PROTOCOL.encode(FORMAT),('localhost',portNum))
+                udpSocket.sendto(DISCONNECT_PROTOCOL.encode(FORMAT),('localhost',ownClientPort))
                 break
             else:
                 myMessage = myMessage
@@ -34,6 +40,8 @@ def recieve_connection_requests(udpServerSocket):        #Method for being alway
         while True:
             request_message, address = udpServerSocket.recvfrom(1024)
             if request_message:
+                if request_message == DISCONNECT_PROTOCOL:
+                    break
                 request_message = request_message.decode(FORMAT)    #Request message for user
                 print(request_message)  #Message to accept interrupts current interaction
                 if ',' in request_message:  #Request information with ports
@@ -51,16 +59,14 @@ def initiate_connection(request_message):
         my_socket.bind(("", ownClientPort))
         print('\nYou are now beginning a chat, send /disconnect if you wish to end')        
         print('\nChat with',arrayOfClientInfo[2])
-        receivingThread = threading.Thread(target=receive_texts, args=(my_socket,arrayOfClientInfo[2],arrayOfClientInfo[2]),daemon=True)
+        receivingThread = threading.Thread(target=receive_texts, args=(my_socket,arrayOfClientInfo[0],arrayOfClientInfo[2]),daemon=True)
         sendingThread = threading.Thread(target=send_texts, args=(my_socket,arrayOfClientInfo[2],int(arrayOfClientInfo[3])),daemon=True)
         receivingThread.start()
         sendingThread.start()
-        while sendingThread.is_alive(): #Have something here to keep the connection here or some method something
-            pass
-        my_socket.close()
 
 def main():
     # TCP socket for initial connection
+    global tcp_client_socket
     tcp_client_socket = socket(AF_INET,SOCK_STREAM)
     tcp_client_socket.connect(('localhost', TCPPORT))
     
@@ -74,17 +80,22 @@ def main():
 
     #Bind the request port
     udpForRequestsPort = ownClientPort+100
+    
+    global udpServerSocket
     udpServerSocket = socket(AF_INET,SOCK_DGRAM) 
     udpServerSocket.bind(('localhost', udpForRequestsPort))
     alwaysAcceptRequests = threading.Thread(target=recieve_connection_requests,args=(udpServerSocket,),daemon=True)
     alwaysAcceptRequests.start()
-    
+    interface()
+        
+def interface():
     print("Here are the messaging options\n\tlist - Gives a list of all available users\n\t/disconnect: terminates communications\nEnter a message to send:")
-    while True:     #Controls termination of server socket
+    engaged = True
+    while engaged:     #Controls termination of server socket
         message = input()
         # Send message to server
         if message == DISCONNECT_PROTOCOL:
-            break
+            engaged = False
         if message == '/accept':
             udpServerSocket.sendto(message.encode(FORMAT),('localhost',12001))
             print('You successfully accepted the chat request')
@@ -98,18 +109,23 @@ def main():
         choice = input ('Would you like to start a chat?\n\ty:yes\n\tn:no\n')
         if choice == 'y':
             message=input('Enter the name of the person you would like to chat with:')
-            tcp_client_socket.send(message.encode(FORMAT))
             print('Please wait for',message,'to accept your chat request')
+            tcp_client_socket.send(message.encode(FORMAT)) 
+        if choice == 'n':
+            print("Here are the messaging options\n\tlist - Gives a list of all available users\n\t/disconnect: terminates communications\nEnter a message to send:")
+        if message == DISCONNECT_PROTOCOL:
+            engaged = False
 
     disconnect(tcp_client_socket,udpServerSocket)
-        
 
 def disconnect(tcp_client_socket,udp_server_socket):
+    # Close sockets on client side
+    udp_server_socket.sendto(DISCONNECT_PROTOCOL.encode(FORMAT),('localhost',ownClientPort+100))
+    udp_server_socket.close()
+    
     #Send disconnect message to the server to close server side
     tcp_client_socket.send(DISCONNECT_PROTOCOL.encode(FORMAT))
     
-    # Close sockets on client side
-    udp_server_socket.close()
     tcp_client_socket.close()
 
 main()
